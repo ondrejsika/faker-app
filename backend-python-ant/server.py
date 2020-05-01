86d917b1d59b5c266db5463ea6df1196af03e7c5
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-01 11:43:15 +0200

    backend-python-ant - add simple prometheus endpoint

--- backend-python-ant/server.py
+++ backend-python-ant/server.py
@@ -10,11 +10,13 @@ import argparse
 import random
 import collections
 import quotes
+import prometheus_client
 
 
 root_parser = argparse.ArgumentParser()
 root_parser.add_argument("--host", type=str, default="0.0.0.0")
 root_parser.add_argument("--port", type=int, default=80)
+root_parser.add_argument("--metrics-port", type=int, default=8080)
 root_parser.add_argument("--instance", type=int, default=0)
 root_parser.add_argument("--wait", type=int)
 root_parser.add_argument("--debug", action="store_true")
@@ -29,6 +31,8 @@ logger.setLevel(logging.DEBUG if args.debug_ws else logging.INFO)
 HOSTNAME = socket.gethostname()
 STARTUP_TIME = int(time.time())
 BACKEND_ID = "%s-%d-%d" % (HOSTNAME, args.instance, STARTUP_TIME)
+
+prom_counter = prometheus_client.Counter("message", "Number of send messages")
 
 CLIENTS = set()
 COUNTER = collections.Counter()
@@ -55,6 +59,7 @@ async def hello(websocket, path):
         while True:
             COUNTER[websocket] += 1
             COUNTER["_total"] += 1
+            prom_counter.inc()
             payload = json.dumps({
                 "backend_impl": "python-ant",
                 "backend_id": BACKEND_ID,
@@ -73,6 +78,7 @@ async def hello(websocket, path):
         unregister(websocket)
 
 logging.info("STARTUP:%s" % BACKEND_ID)
+prometheus_client.start_http_server(args.metrics_port)
 start_server = websockets.serve(hello, args.host, args.port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
