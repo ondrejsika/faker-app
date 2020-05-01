c01c30e7c2d9fa3dd523ecd1d98536412b58c7d3
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-01 10:33:56 +0200

    Add Python Ant backend & Python client

---
+++ backend-python-ant/server.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import asyncio
+import json
+import websockets
+import socket
+import time
+import logging
+import argparse
+import random
+import collections
+
+root_parser = argparse.ArgumentParser()
+root_parser.add_argument("--host", type=str, default="0.0.0.0")
+root_parser.add_argument("--port", type=int, default=80)
+root_parser.add_argument("--instance", type=int, default=0)
+root_parser.add_argument("--wait", type=int)
+root_parser.add_argument("--debug", action="store_true")
+root_parser.add_argument("--debug-ws", action="store_true")
+
+args = root_parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.DEBUG if args.debug_ws else logging.INFO)
+
+HOSTNAME = socket.gethostname()
+STARTUP_TIME = int(time.time())
+BACKEND_ID = "%s-%d-%d" % (HOSTNAME, args.instance, STARTUP_TIME)
+
+CLIENTS = set()
+COUNTER = collections.Counter()
+
+
+def register(websocket):
+    CLIENTS.add(websocket)
+    logging.info("CONNECTED:%s:%d" %
+                 (websocket.remote_address[0], websocket.remote_address[1]))
+
+
+def unregister(websocket):
+    CLIENTS.remove(websocket)
+    logging.info("DISCONNECTED:%s:%d" %
+                 (websocket.remote_address[0], websocket.remote_address[1]))
+
+
+async def hello(websocket, path):
+    register(websocket)
+    try:
+        while True:
+            COUNTER[websocket] += 1
+            COUNTER["_total"] += 1
+            payload = json.dumps({
+                "backend_impl": "python-ant",
+                "backend_id": BACKEND_ID,
+                "clients_count": len(CLIENTS),
+                "counter": COUNTER[websocket],
+                "counter_total": COUNTER["_total"],
+                "timestamp": int(time.time()),
+                "message": "Hello world from %s" % HOSTNAME,
+            })
+            await websocket.send(payload)
+            logging.debug("%s %s" % (BACKEND_ID, payload))
+            wait = (args.wait or random.randint(100, 1000)) / 1000.
+            await asyncio.sleep(wait)
+
+    finally:
+        unregister(websocket)
+
+logging.info("STARTUP:%s" % BACKEND_ID)
+start_server = websockets.serve(hello, args.host, args.port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ client-python/client.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import argparse
+import logging
+import json
+
+root_parser = argparse.ArgumentParser()
+root_parser.add_argument(
+    '-c', '--client', action='append', type=str, required=True)
+root_parser.add_argument("--debug", action="store_true")
+root_parser.add_argument("--debug-ws", action="store_true")
+
+args = root_parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.DEBUG if args.debug_ws else logging.INFO)
+
+
+async def hello(client):
+    uri = "ws://%s" % client
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                logging.info("CONNECTED:%s:%d" % (
+                    websocket.remote_address[0], websocket.remote_address[1]))
+                try:
+                    while True:
+                        payload_str = await websocket.recv()
+                        payload = json.loads(payload_str)
+                        logging.debug(payload_str)
+                        if payload["counter"] % 1000 == 0:
+                            logging.info(payload_str)
+
+                except websockets.exceptions.ConnectionClosedError as e:
+                    pass
+        except OSError as e:
+            pass
+        await asyncio.sleep(1)
+
+
+loop = asyncio.get_event_loop()
+for client in args.client:
+    loop.create_task(hello(client))
+loop.run_forever()
